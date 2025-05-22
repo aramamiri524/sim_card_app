2abe61e485dcba7c84c52077cf08526606a230ce
amiriaram524@gmail.com <amiriaram524@gmail.com>
2025-05-22 09:00:35 -0700

    upload

--- project_python/sim_info.py
+++ project_python/sim_info.py
@@ -6,16 +6,15 @@ from validator import *
 
 sim_list = read_from_file("sims.dat")
 
-#بارگذاری داده ها در جدول
-def load_data(sim_list):
-    sim_list = read_from_file("sims.dat")
+# بارگذاری داده ها در جدول
+def load_data():
     for row in table.get_children():
         table.delete(row)
 
     for sim in sim_list:
         table.insert("", END, values=sim)
 
-#بازنشانی فرم(آیدی جدید تولید میشود و بقیه ی موارد خالی یا صفر میشوند ،جدول دوباره باز نشانی میشود)
+# بازنشانی فرم
 def reset_form():
     id.set(len(sim_list) + 1)
     number.set("")
@@ -23,9 +22,9 @@ def reset_form():
     price.set(0)
     charge.set(0)
     owner.set("")
-    load_data(sim_list)
+    load_data()
 
-#ذخیره سازی اطلاعات جدید
+# ذخیره سازی اطلاعات جدید
 def save_btn_click():
     sim = (id.get(), number.get(), operator.get(), price.get(), charge.get(), owner.get())
     errors = sim_validator(sim)
@@ -37,7 +36,7 @@ def save_btn_click():
         write_to_file("sims.dat", sim_list)
         reset_form()
 
-#انتخاب ردیف از جدول برای ویرایش و بررسی
+# انتخاب ردیف از جدول برای ویرایش
 def table_select(x):
     selected_sim = table.item(table.focus())["values"]
     if selected_sim:
@@ -48,43 +47,61 @@ def table_select(x):
         charge.set(selected_sim[4])
         owner.set(selected_sim[5])
 
-#تابع ویرایش
+# تابع ویرایش
 def edit_btn_click():
-    pass
+    selected_sim = table.item(table.focus())["values"]
+    if selected_sim:
+        sim = (id.get(), number.get(), operator.get(), price.get(), charge.get(), owner.get())
+        errors = sim_validator(sim)
+        if errors:
+            msg.showerror("Errors", "\n".join(errors))
+        else:
+            for index, sim_data in enumerate(sim_list):
+                if sim_data[0] == selected_sim[0]:
+                    sim_list[index] = sim
+                    break
+            write_to_file("sims.dat", sim_list)
+            load_data()
+            msg.showinfo("Updated", "Sim card updated")
 
-#تابع حذف
+# تابع حذف
 def remove_btn_click():
-    pass
+    selected_sim = table.item(table.focus())["values"]
+    if selected_sim:
+        sim_list.remove(selected_sim)
+        write_to_file("sims.dat", sim_list)
+        load_data()
+        msg.showinfo("Removed", "Sim card removed")
 
-#یک پنجره اصلی با عنوان و اندازه مشخص ساخته میشود
+# یک پنجره اصلی با عنوان و اندازه مشخص ساخته میشود
 window = Tk()
 window.title("Sim Card Registration")
-window.geometry("780x320")
+window.geometry("920x350")
 
 # تعریف و جایگذاری فایلها
 Label(window, text="Id").place(x=20, y=20)
 id = IntVar(value=1)
-Entry(window, textvariable=id, state="readonly").place(x=80, y=20)
+Entry(window, textvariable=id, state="readonly",width=32).place(x=80, y=20)
 
 Label(window, text="Number").place(x=20, y=60)
 number = StringVar()
-Entry(window, textvariable=number).place(x=80, y=60)
+Entry(window, textvariable=number,width=32).place(x=80, y=60)
 
 Label(window, text="Operator").place(x=20, y=100)
 operator = StringVar()
-Entry(window, textvariable=operator).place(x=80, y=100)
+Entry(window, textvariable=operator,width=32).place(x=80, y=100)
 
 Label(window, text="Price").place(x=20, y=140)
 price = IntVar()
-Entry(window, textvariable=price).place(x=80, y=140)
+Entry(window, textvariable=price, width=32).place(x=80, y=140)
 
 Label(window, text="Charge").place(x=20, y=180)
 charge = IntVar()
-Entry(window, textvariable=charge).place(x=80, y=180)
+Entry(window, textvariable=charge,width=32).place(x=80, y=180)
 
 Label(window, text="Owner").place(x=20, y=220)
 owner = StringVar()
-Entry(window, textvariable=owner).place(x=80, y=220)
+Entry(window, textvariable=owner,width=32).place(x=80, y=220)
 
 # Table
 table = ttk.Treeview(window, columns=[1, 2, 3, 4, 5, 6], show="headings")
@@ -102,10 +119,10 @@ table.bind("<<TreeviewSelect>>", table_select)
 table.place(x=300, y=20)
 
 # Buttons
-Button(window, text="Save", width=8, command=save_btn_click).place(x=20, y=270)
-Button(window, text="Edit", width=8, command=edit_btn_click).place(x=110, y=270)
-Button(window, text="Remove", width=8, command=remove_btn_click).place(x=200, y=270)
-Button(window, text="Clear", width=8, command=reset_form).place(x=20, y=240, width=190)
+Button(window, text="Save", width=8, command=save_btn_click).place(x=20, y=300)
+Button(window, text="Edit", width=8, command=edit_btn_click).place(x=110, y=300)
+Button(window, text="Remove", width=8, command=remove_btn_click).place(x=210, y=300)
+Button(window, text="Clear", width=8, command=reset_form).place(x=20, y=260, width=260)
 
 reset_form()
 window.mainloop()
